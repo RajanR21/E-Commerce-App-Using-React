38a83104aa24ab2a3bc76190253e1ed1952f3c15
Rajan Ravisaheb <rajanroy220@gmail.com>
2024-03-18 15:44:48 +0530

    menu bar

---
+++ src/components/products/Menu.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function MyForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <select {...register("selectField")} defaultValue="">
+        <option value="" disabled>
+          Select an option
+        </option>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+      {errors.selectField && <span>This field is required</span>}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default MyForm;
